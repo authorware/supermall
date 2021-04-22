55c9f6b817e211cc37531b177f653a75531f37e4
Simon <shihczhang@gmail.com>
2021-04-22 20:49:59 +0800

    init supermall

---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <slot v-if="!isActive" name="item-icon"></slot>
+    <slot v-else name="item-icon-active"></slot>
+    <div :style="activateStyle">
+      <slot  name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'red'
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    isActive () {
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activateStyle () {
+      return this.isActive ? {color: this.activeColor} : { }
+    }
+  },
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+
+.active {
+  color: red;
+}
+</style>
