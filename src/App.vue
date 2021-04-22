a92b4f09faf3e7a15a49622db6cfdb80ac646378
Simon <shihczhang@gmail.com>
2021-04-22 22:30:29 +0800

    init supermall

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <router-view></router-view>
-    <main-tab-bar></main-tab-bar>
+    <router-view/>
+    <main-tab-bar/>
   </div>
 </template>
 
 <script>
-import MainTabBar from "./components/mainTabbar/MainTabBar";
+import MainTabBar from "components/content/mainTabbar/MainTabBar";
 export default {
   name: 'App',
   components: {
@@ -16,6 +16,6 @@ export default {
 </script>
 
 <style>
-  @import "./assets/css/base.css";
+  @import "assets/css/base.css";
 
 </style>
