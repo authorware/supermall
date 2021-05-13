d8a13932832ed7888149abec02f37f9333cd1137
Simon <shihczhang@gmail.com>
2021-05-13 20:26:48 +0800

    init supermall

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,27 +3,103 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class=tab-control :titles="['流行','新款','精选']"></tab-control>
+
+    <ul>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import NavBar from "components/common/navbar/NavBar";
   import HomeSwiper from "./childComps/HomeSwiper";
   import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView";
+
+  import NavBar from "components/common/navbar/NavBar";
+  import TabControl from "components/content/tabControl/TabControl";
+
   import {getHomeMultidata} from "network/home";
 
 
   export default {
     name: "Home",
     components: {
-      NavBar,
       HomeSwiper,
-      RecommendView
+      RecommendView,
+      FeatureView,
+      NavBar,
+      TabControl
     },
     data() {
       return {
         banners: [],
-        recommends: []
+        recommends: [],
+        goods: {
+          'pop': { page: 0, list: []},
+          'new': { page: 0, list: []},
+          'sell': { page: 0, list: []}
+        }
       }
     },
     created() {
@@ -37,8 +113,22 @@
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
   .home-nav {
     background-color: var(--color-tint);
     color: #ffffff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="feature">
+    <a href="www.baidu.com"></a>
+      <img src="~assets/img/home/recommend_bg.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeatureView"
+}
+</script>
+
+<style scoped>
+  .feature img {
+    width: 100%;
+  }
+</style>
