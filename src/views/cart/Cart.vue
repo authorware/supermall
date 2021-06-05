44c6350057fe9672490aafcdab8444042fecc8f6
Simon <shihczhang@gmail.com>
2021-06-05 19:53:49 +0800

    init supermall

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -3,30 +3,35 @@
     <nav-bar class="nav-bar">
       <div slot="center">购物车({{cartLength}})</div>
     </nav-bar>
+    <cart-list></cart-list>
   </div>
 </template>
 
 
 <script>
 import NavBar from "components/common/navbar/NavBar"
+import CartList from "./childComps/CartList";
 
 import { mapGetters } from 'vuex'
 
 export default {
   name: "Cart",
   components: {
+    CartList,
     NavBar
   },
   computed: {
     ...mapGetters([
-      'cartLength',
-      'cartList'
+      'cartLength'
     ])
   }
 }
 </script>
 
 <style scoped>
+  .cart {
+    height: 100vh;
+  }
   .nav-bar {
     background-color: var(--color-tint);
     color: #ffffff;
---
+++ src/views/cart/childComps/CartList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="cart-list">
+    <scroll class="content">
+      <li v-for="item in cartList">{{item}}</li>
+    </scroll>
+  </div>
+</template>
+
+<script>
+import Scroll from "components/common/scroll/Scroll";
+import {mapGetters} from "vuex";
+export default {
+  name: "CartList",
+  components: {
+    Scroll
+  },
+  computed: {
+    ...mapGetters([
+      'cartList'
+    ])
+  }
+}
+</script>
+
+<style scoped>
+  .cart-list{
+    height: calc(100% - 44px - 49px);
+  }
+  .content{
+    height: 100%;
+    overflow: hidden;
+  }
+</style>
